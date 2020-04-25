9398944737e5d513f07de8a7f37244a436a28990
皮卡丘 <liu1365102044@163.com>
2020-04-25 19:33:17 +0800

    1.在.pch添加部分常用方法声明 2.添加log文件 3.修改MainSingletion中的方法为静态方法，不再使用单利 4.添加几个插件，还未pod 到本地

--- zhuangBei/ZhuangBei/commen/aHelpers/MainSingleton.h
+++ zhuangBei/ZhuangBei/commen/aHelpers/MainSingleton.h
@@ -10,24 +10,25 @@
 #import <UIKit/UIKit.h>
 
 @interface MainSingleton : NSObject
-+ (instancetype) defaultSingleton;
+//+ (instancetype) defaultSingleton;
 
-//字符串是否为空
-BOOL stringIsNotEmpty (NSString * str);
-BOOL stringIsEmpty (NSString * str);
+////字符串是否为空
++ (BOOL)stringIsNotEmpty:(NSString *)str;
++ (BOOL)stringIsEmpty:(NSString *)str;
 
 //时间转换
-- (NSString *) dateToString:(NSDate *)date;
-- (NSDate *) stringToDate:(NSString *)dateStr;
-- (NSString *) timeToString:(time_t)time;
++ (NSString *) dateToString:(NSDate *)date;
++ (NSDate *) stringToDate:(NSString *)dateStr;
++ (NSString *) timeToString:(time_t)time;
 
 //获取时间
-- (NSString *) getTime;
-- (NSString *) getDay;
-- (NSString *) intervalFromLastDate: (NSString *) dateString1 toTheDate:(NSString *) dateString2;
++ (NSString *) getTime;
++ (NSString *) getDay;
++ (NSString *) intervalFromLastDate: (NSString *) dateString1 toTheDate:(NSString *) dateString2;
 
 //颜色转换
-- (UIColor *)colorWithHexString:(NSString *)color alpha:(CGFloat)alpha;
++ (UIColor *)colorWithHexString:(NSString *)color alpha:(CGFloat)alpha;
+
 @end
 
 
